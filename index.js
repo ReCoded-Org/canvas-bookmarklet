b8c1864f6ad84bf3c71823a37ad6bdb7b82e3f26
Wisam Naji <guitar90rocker@gmail.com>
2021-08-25 18:14:26 +0000

    testing the script

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 (function () {
-  alert(window.location);
+  const location = window.location.split('/')
+  if(!location.includes('courses') && !location.includes('gradebook')) {
+    alert("This tool can be used in Canvas only. Please head to your course grades page in Canvas dashboard.")
+    return;
+  }
+
+  const cidRgx = /courses\/(\d+)\/gradebook/g
+  let courseId = window.location.pathname.match(cidRgx).pop()
+  if (parseInt(courseId) === NaN) {
+    courseId = prompt("Cannot find course id! Please enter it below. It should be in the URL of this page")
+  }
+
+  let uids = []
+  let students = [];
+  let assignments = [];
+
+  fetch(`https://my.learn.co/courses/${courseId}/gradebook/user_ids`).then(r => r.json()).then(data => {
+    uids = data.user_ids
+    console.log(uids)
+  })
+
 })();

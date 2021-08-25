2d211ba297f34ed062306441697b23a0c567722b
Wisam Naji <guitar90rocker@gmail.com>
2021-08-25 18:18:03 +0000

    fixed code issue

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 (function () {
-  const location = window.location.split('/')
-  if(!location.includes('courses') && !location.includes('gradebook')) {
-    alert("This tool can be used in Canvas only. Please head to your course grades page in Canvas dashboard.")
+  const location = window.location;
+  const locationRegx = /courses\/(\d+)\/gradebook/;
+  const matches = location.pathname.match(locationRegx);
+  if (matches.length <= 0) {
+    alert(
+      "This tool can be used in Canvas only. Please head to your course grades page in Canvas dashboard."
+    );
     return;
   }
-
-  const cidRgx = /courses\/(\d+)\/gradebook/g
-  let courseId = window.location.pathname.match(cidRgx).pop()
+  
+  let courseId = matches.pop();
   if (parseInt(courseId) === NaN) {
-    courseId = prompt("Cannot find course id! Please enter it below. It should be in the URL of this page")
+    courseId = prompt(
+      "Cannot find course id! Please enter it below. It should be in the URL of this page"
+    );
   }
 
-  let uids = []
+  let uids = [];
   let students = [];
   let assignments = [];
 
-  fetch(`https://my.learn.co/courses/${courseId}/gradebook/user_ids`).then(r => r.json()).then(data => {
-    uids = data.user_ids
-    console.log(uids)
-  })
-
+  fetch(`https://my.learn.co/courses/${courseId}/gradebook/user_ids`)
+    .then((r) => r.json())
+    .then((data) => {
+      uids = data.user_ids;
+      console.log(uids);
+    });
 })();
